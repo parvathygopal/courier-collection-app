436457b1b0194a72ead9e7f9f33f0193cd607f3f
parvathyg <parvathyg@qburst.com>
2026-06-22 12:02:17 +0530

    webhook create package implementation in logistics app

--- stage2-logistics-app/backend/src/app.ts
+++ stage2-logistics-app/backend/src/app.ts
@@ -4,12 +4,13 @@ import bagRoutes from "./routes/bag.route.js";
 import truckRoutes from "./routes/truck.route.js";
 import dashboardRoutes from "./routes/dasboard.routes.js";
 import regionRoutes from "./routes/region.route.js";
+import webhookRoutes from "./routes/webhook.route.js";
 import { errorHandler } from "./middlewares/error.middleware.js";
 import cors from "cors";
 
 const app = express();
 
-const FRONTEND_ORIGINS = ["http://localhost:5173", "http://127.0.0.1:5173"];
+const FRONTEND_ORIGINS = ["http://localhost:5174", "http://127.0.0.1:5174"];
 
 app.use(
   cors({
@@ -25,6 +26,7 @@ app.use("/bags", bagRoutes);
 app.use("/trucks", truckRoutes);
 app.use("/dashboard", dashboardRoutes);
 app.use("/regions", regionRoutes);
+app.use("/webhook", webhookRoutes);
 
 app.get("/", (req, res) => {
   res.send("Hello from courier-logistics-app");
--- stage2-logistics-app/backend/src/index.ts
+++ stage2-logistics-app/backend/src/index.ts
@@ -3,9 +3,12 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import app from "./app.js";
+import { startPushEtlJob } from "./services/etl.service.js";
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`Server running on port ${PORT}`);
+  startPushEtlJob();
 });
